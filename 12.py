0ae5e6c354e02185df569ea8b84d5335a6877278
Pedro Gonçalves <131919559+pedrora012@users.noreply.github.com>
2023-04-29 18:35:51 -0300

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,10 @@
+#Exercício 12
+T = int(input())
+A,B,C,D,E = input().split(' ')
+A = int(A)
+B = int(B)
+C = int(C)
+D = int(D)
+E = int(E)
+respostas = [A,B,C,D,E]
+print(respostas.count(T))
---
+++ 20.py
@@ -0,0 +1,8 @@
+#Exercício 20
+d = int(input())
+if d <= 800:
+    print(1)
+elif d <= 1400:
+    print(2)
+else:
+    print(3)
